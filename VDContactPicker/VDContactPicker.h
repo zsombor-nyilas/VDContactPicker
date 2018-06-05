321248c3ef252b1fe5f8ba04ada6fca785dc02eb
Vishal Deep Kanojia <vishal.deep@live.com>
2018-06-05 16:23:51 +0530

    Added support for Carthage

---
+++ VDContactPicker/VDContactPicker.h
@@ -0,0 +1,19 @@
+//
+//  VDContactPicker.h
+//  VDContactPicker
+//
+//  Created by Vishal on 05/06/18.
+//  Copyright © 2018 Vishal Deep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VDContactPicker.
+FOUNDATION_EXPORT double VDContactPickerVersionNumber;
+
+//! Project version string for VDContactPicker.
+FOUNDATION_EXPORT const unsigned char VDContactPickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VDContactPicker/PublicHeader.h>
+
+
